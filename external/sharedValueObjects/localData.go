9b91fd6cb85af0a85ccb92b8214c850886030c2a
Karim <k.mohamed@hqolsa.com>
2024-05-02 02:06:35 +0300

    commitWithAddVariantMessageToLocalData

--- external/sharedValueObjects/localData.go
+++ external/sharedValueObjects/localData.go
@@ -8,26 +8,29 @@ import (
 
 // Define a generic struct LocalData
 type LocalData struct {
-	Name        string
-	Slug        string
-	Description string
+	Name           string
+	Slug           string
+	Description    string
+	VariantMessage string
 }
 
 // Implement MarshalJSON method for LocalData
 func (ld *LocalData) MarshalJSON() ([]byte, error) {
 	return json.Marshal(map[string]interface{}{
-		"name":        ld.Name,
-		"slug":        ld.Slug,
-		"description": ld.Description,
+		"name":            ld.Name,
+		"slug":            ld.Slug,
+		"description":     ld.Description,
+		"variant_message": ld.VariantMessage,
 	})
 }
 
 // Implement UnmarshalJSON method for LocalData
 func (ld *LocalData) UnmarshalJSON(data []byte) error {
 	var lData struct {
-		Name        string `json:"name" validate:"required"`
-		Slug        string `json:"slug" validate:"required"`
-		Description string `json:"description" validate:"required"`
+		Name           string `json:"name" validate:"required"`
+		Slug           string `json:"slug" validate:"required"`
+		Description    string `json:"description" validate:"required"`
+		VariantMessage string `json:"variant_message" validate:"required"`
 	}
 
 	if err := json.Unmarshal(data, &lData); err != nil {
@@ -44,5 +47,6 @@ func (ld *LocalData) UnmarshalJSON(data []byte) error {
 	ld.Name = lData.Name
 	ld.Slug = lData.Slug
 	ld.Description = lData.Description
+	ld.VariantMessage = lData.VariantMessage
 	return nil
 }
